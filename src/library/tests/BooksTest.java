1eded02df7fc6990f1377e479f962f1eca64eb10
Johan Lipecki <lipecki@kth.se>
2016-09-26 10:30:41 +0200

    flyttar filhantering á la C till testmappen

--- src/library/tests/BooksTest.java
+++ src/library/tests/BooksTest.java
@@ -80,4 +80,26 @@ public class BooksTest {
         return library;
         
     }
+    
+     /**
+     * Parses Book string s 
+     * @return Book    
+     * @param s 
+     */
+    public static Book convertStringToBook(String s) throws IndexOutOfBoundsException {
+        ArrayList<String> book= new ArrayList();
+        book.addAll(Arrays.asList(s.split(";", 10)));
+        if(book.size()<4){
+            System.err.println("Too few parameters in list");
+            throw new IndexOutOfBoundsException();
+        }
+        Book thisOne = new Book(book.get(0),book.get(1),Integer.decode(book.get(2)),Double.valueOf(book.get(3)));
+        
+        if(book.size()>4) 
+            for (String z : book.subList(4, book.size())) {
+                thisOne.addAuthor(new Author(z.trim()));
+        }
+        return thisOne;
+        
+    }
 }
